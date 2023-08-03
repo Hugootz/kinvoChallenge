0995448f3d8d60f0e71f9bf6904f36d587329913
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-03 18:49:58 -0300

    tema global e mudança de componente

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Challenge } from "./src/screens/challenge";
+import { Desafio } from "./src/screens/Desafio";
 
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/theme";
 export default function App() {
   return (
-    <NavigationContainer>
-      <Challenge />
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Desafio />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
