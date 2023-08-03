2add5baf91bf2932f5c9f23f571f800ba527e519
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-03 18:44:55 -0300

    criação tela ações

---
+++ src/screens/Ações/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export function Ações() {
+  return (
+    <Container>
+      <Title>AÇÕES</Title>
+    </Container>
+  );
+}
