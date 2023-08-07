97d1360cb0e8f76a59880a9f800c0407fa5c9e04
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-07 17:23:19 -0300

    estilizaçãoCardAções

---
+++ src/Components/CardAções/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+export const WrapperCard = styled.View`
+  background-color: white;
+  width: 350px;
+  height: 230px;
+  border-radius: 10px;
+`;
+export const Lock = styled.View``;
+export const Title = styled.Text``;
+export const SecondTitle = styled.Text``;
