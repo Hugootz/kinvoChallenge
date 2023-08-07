ad8a810ccb5fdbd99ad24274c8d1911b383f6ca9
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-07 17:14:41 -0300

    introdução das rotas no app.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Desafio } from "./src/screens/Desafio";
-
+import AppRoutes from "./src/Routes/Router";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/theme";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Desafio />
+        <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
   );
