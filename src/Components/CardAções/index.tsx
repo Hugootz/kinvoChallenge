9f2919c46a5760233e41945f7706f444e996f334
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-07 17:22:46 -0300

    criação do componente CardAções

---
+++ src/Components/CardAções/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, WrapperCard, Lock, Title, SecondTitle } from "./styles";
+
+export function CardAções() {
+  return (
+    <Container>
+      <WrapperCard>
+        <Lock>
+          <Title></Title>
+          <SecondTitle></SecondTitle>
+        </Lock>
+      </WrapperCard>
+    </Container>
+  );
+}
