1d5b32b6a6683cf978ab01df892cc0542fea2fa8
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-08 11:23:57 -0300

    estilização tela desafio

--- src/screens/Desafio/styles.ts
+++ src/screens/Desafio/styles.ts
@@ -1,15 +1,6 @@
-import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-`;
-export const Title = styled.Text`
-  margin-left: 30px;
-  margin-top: 70px;
-  bottom: 15px;
-  color: ${({ theme }) => theme.colors.purple_title};
-  font-weight: bold;
-  font-size: ${RFValue(20)}px;
 `;
