274aa7430242be732d8f5381d25a42347cab187c
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-11 18:09:19 -0300

    remove previdência

--- src/screens/Previdência/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-import { RFValue } from "react-native-responsive-fontsize";
-
-export const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-export const Title = styled.Text``;
