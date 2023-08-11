61f3bc8316ba07a5b2366d13d3ae3f69cbe01a3d
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-11 18:07:21 -0300

    tela previdencia

---
+++ src/screens/Previdencia/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export function Previdencia() {
+  return (
+    <Container>
+      <Title>PREVIDÊNCIA</Title>
+    </Container>
+  );
+}
