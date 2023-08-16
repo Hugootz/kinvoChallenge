6b7324afebe99beabb3c7155e5c99c35883c14ee
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-16 16:24:22 -0300

    tipagem da flatlist

--- src/screens/Acoes/styles.ts
+++ src/screens/Acoes/styles.ts
@@ -1,12 +1,11 @@
+import { ResponseApi } from "./../../@types/api";
 import styled from "styled-components/native";
 import { FlatList } from "react-native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 `;
-export const Title = styled.Text``;
+
 export const ListCard = styled(FlatList).attrs({
-  contentContainerStyle: {},
-})`` as React.ComponentType as new <DataListProps>() => FlatList<DataListProps>;
+  contentContainerStyle: { padding: 20 },
+})`` as React.ComponentType as new <ResponseApi>() => FlatList<ResponseApi>;
