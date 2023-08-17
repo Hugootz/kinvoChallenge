36c11c96c9b442c58ff914c17ff5960f0881bee2
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-17 16:00:27 -0300

    styled componente ações

---
+++ src/screens/Ações/styles.ts
@@ -0,0 +1,11 @@
+import { ResponseApi } from "../../@types/api";
+import styled from "styled-components/native";
+import { FlatList } from "react-native";
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const ListCard = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 20 },
+})`` as React.ComponentType as new <ResponseApi>() => FlatList<ResponseApi>;
