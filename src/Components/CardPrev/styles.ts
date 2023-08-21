f98c8fa0a77452926a2b0c59d219952b43fd39e4
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-21 20:08:27 -0300

    styled do CardPrev

---
+++ src/Components/CardPrev/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+export const Details = styled.View``;
+export const AgroupTitle = styled.View``;
+export const Name = styled.Text``;
+export const Ticker = styled.Text``;
+export const WrapperDetails = styled.View``;
+export const MinimumValue = styled.View``;
+export const Profitability = styled.View``;
