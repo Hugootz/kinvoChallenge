be1d6e912d4966605fc7df7b278b4a128d40808f
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-21 19:58:40 -0300

    tipagem resposta api tela previdência

--- src/@types/api.ts
+++ src/@types/api.ts
@@ -1,7 +1,15 @@
-export interface ResponseApi {
+export interface ResponseApiAcoes {
   id: number;
   name: string;
   ticker: string;
   minimumValue: number;
+  profitability: number;
+}
+export interface ResponseApiPrev {
+  name: string;
+  type: string;
+  minimumValue: number;
+  tax: number;
+  redemptionTerm: number;
   profitability: number;
 }
