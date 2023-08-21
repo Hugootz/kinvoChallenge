3260f21b30a9b7cbf0c38236cb465cf2a158302b
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-21 20:09:56 -0300

    lógica para ordernar lista em ordem alfabetica

--- src/screens/Ações/index.tsx
+++ src/screens/Ações/index.tsx
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from "react";
 import { Container, ListCard } from "./styles";
 import { CardAction } from "../../Components/CardAction";
 import axios from "axios";
-import { ResponseApi } from "../../@types/api";
+import { ResponseApiAcoes } from "../../@types/api";
 import { Load } from "../../Components/Load";
 
-export function Ações() {
-  const [list, setList] = useState<ResponseApi[]>([]);
+export function Acoes() {
+  const [list, setList] = useState<ResponseApiAcoes[]>([]);
   const [loading, setLoading] = useState(true);
+
   async function getApi() {
     try {
       const response = await axios.get(
         "https://6266f62263e0f382568936e4.mockapi.io/stocks"
       );
-      setList(response.data.data);
+      setList(
+        response.data.data.sort((a, b) =>
+          a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+        )
+      );
     } catch (error) {
       console.log(error);
     } finally {
@@ -30,6 +35,7 @@ export function Ações() {
         <Load />
       ) : (
         <ListCard
+          showsVerticalScrollIndicator={false}
           data={list}
           keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => <CardAction data={item} />}
