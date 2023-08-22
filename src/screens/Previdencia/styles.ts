f6998e2517accb9b23a3be075761778b0fae4365
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-22 16:38:41 -0300

    styled da tela previdencia

--- src/screens/Previdencia/styles.ts
+++ src/screens/Previdencia/styles.ts
@@ -1,9 +1,28 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
-
+import { FlatList, TouchableOpacity } from "react-native";
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-export const Title = styled.Text``;
+export const DetailsStyle = styled.View`
+  width: 100px;
+  height: 40px;
+  background-color: ${({ theme }) => theme.colors.light};
+  top: 15px;
+  flex-direction: row;
+  border-radius: 17px;
+  margin-bottom: 30px;
+`;
+export const ButtonWrapper = styled(TouchableOpacity)``;
+export const Filter = styled.Text`
+  padding: 10px;
+  left: 4px;
+`;
+
+export const ListPrev = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 20 },
+})`` as React.ComponentType as new <
+  ResponseApiPrev
+>() => FlatList<ResponseApiPrev>;
