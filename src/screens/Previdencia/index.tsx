263a9be9da2a37df2ea099f11c3920fe35e38490
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-22 16:37:49 -0300

    tela previdencia, chamada api e estrutura JSX

--- src/screens/Previdencia/index.tsx
+++ src/screens/Previdencia/index.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { Container, Title } from "./styles";
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  DetailsStyle,
+  ButtonWrapper,
+  Filter,
+  ListPrev,
+} from "./styles";
+import { ResponseApiPrev } from "../../@types/typesApi";
+import { CardPrev } from "../../Components/CardPrev";
+import { Load } from "../../Components/Load";
+import api from "../../services/api";
 
 export function Previdencia() {
+  const [prevList, setPrevList] = useState<ResponseApiPrev[]>([]);
+  const [loading, setLoading] = useState(true);
+  async function getApiPrev() {
+    try {
+      const responsePrev = await api.get("/pension");
+      setPrevList(responsePrev.data.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getApiPrev();
+  }, []);
   return (
     <Container>
-      <Title>PREVIDÊNCIA</Title>
+      <ButtonWrapper>
+        <DetailsStyle>
+          <Filter>SEM TAXA</Filter>
+        </DetailsStyle>
+      </ButtonWrapper>
+
+      {loading ? (
+        <Load />
+      ) : (
+        <ListPrev
+          showsVerticalScrollIndicator={false}
+          data={prevList}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <CardPrev dataPrev={item} />}
+        />
+      )}
     </Container>
   );
 }
