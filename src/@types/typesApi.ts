6341db1fd302cb4fb46a1bed95cc12240d1f0359
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-29 18:16:25 -0300

    tipagem da resposta da api e cores do botão

--- src/@types/typesApi.ts
+++ src/@types/typesApi.ts
@@ -14,3 +14,30 @@ export interface ResponseApiPrev {
   redemptionTerm: number;
   profitability: number;
 }
+export interface ResponseApiFundos {
+  id: number;
+  name: string;
+  type: string;
+  minimumValue: number;
+  rating: number;
+  profitability: number;
+  status: number;
+}
+export interface ButtonStyle {
+  detailsStyle: { backgroundColor: string };
+  filter: { color: string };
+}
+export interface ButtonVariant {
+  enabled: ButtonStyle;
+  disabled: ButtonStyle;
+}
+export const colorChange = {
+  enabled: {
+    backgroundColor: "#000000",
+    color: "#ffffffff",
+  },
+  disabled: {
+    backgroundColor: "#ffffffff",
+    color: "#000000",
+  },
+};
