2f5a7f6aac4ccedbe45cf127766087c18b4492c7
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-29 18:19:58 -0300

    estilização do buttonFilter

--- src/Components/ButtonFilter/styles.ts
+++ src/Components/ButtonFilter/styles.ts
@@ -1,12 +1,15 @@
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-
+import {colorChange} from '../../@types/typesApi'
 export const Container = styled.View``;
-export const ButtonWrapper = styled(TouchableOpacity)``;
+export const ButtonWrapper = styled(TouchableOpacity)`
+
+`;
 export const DetailsStyle = styled.View`
   width: 100px;
   height: 40px;
   background-color: ${({ theme }) => theme.colors.light};
+
   top: 15px;
   border-radius: 17px;
   margin-bottom: 30px;
@@ -14,6 +17,7 @@ export const DetailsStyle = styled.View`
 `;
 export const Filter = styled.Text`
   padding: 10px;
+  color: ${({ theme }) => theme.colors.dark};
 
   font-weight: bold;
   align-self: center;
