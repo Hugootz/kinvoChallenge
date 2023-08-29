6228ab2d460a760ed2ca780c2ed7b6189f77664a
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-29 18:22:48 -0300

    tela fundos

--- src/screens/Fundos/index.tsx
+++ src/screens/Fundos/index.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-import { Container, Title } from "./styles";
-
+import React, { useState, useEffect } from "react";
+import { Container, ListFunds } from "./styles";
+import { ResponseApiFundos } from "../../@types/typesApi";
+import { CardFundos } from "../../Components/CardFundos";
+import { Load } from "../../Components/Load";
+import api from "../../services/api";
 export function Fundos() {
+  const [listFunds, setListFunds] = useState<ResponseApiFundos[]>([]);
+  const [loadingFunds, setLoadingFunds] = useState(true);
+  async function getApi() {
+    try {
+      const responseF = await api.get("/funds");
+      setListFunds(responseF.data.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingFunds(false);
+    }
+  }
+  useEffect(() => {
+    getApi;
+  }, []);
   return (
     <Container>
-      <Title>FUNDOS</Title>
+      {loadingFunds ? (
+        <Load />
+      ) : (
+        <ListFunds
+          data={listFunds}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <CardFundos fundos={item} />}
+        />
+      )}
     </Container>
   );
 }
