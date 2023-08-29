b2ea2b1c5abd3245ed083ba5f6db3d3823afca45
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-29 18:23:22 -0300

    tipagem da flatlist

--- src/screens/Fundos/styles.ts
+++ src/screens/Fundos/styles.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { FlatList } from "react-native";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-export const Title = styled.Text``;
+export const ListFunds = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 20 },
+})`` as React.ComponentType as new <
+  ResponseApiFundos
+>() => FlatList<ResponseApiFundos>;
