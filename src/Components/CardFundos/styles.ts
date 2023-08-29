6281073964c07063097bff37fc11d2047179ff1e
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-08-29 18:21:23 -0300

    estilização do CardFundos

---
+++ src/Components/CardFundos/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+export const Details = styled.View``;
+export const AgroupTitle = styled.View``;
+export const Name = styled.Text``;
+export const Type = styled.Text``;
+export const WrapperDetails = styled.View``;
+export const MinimumValue = styled.Text``;
+export const Rating = styled.Text``;
+export const Profitability = styled.Text``;
+export const Status = styled.Text``;
