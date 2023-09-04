da62d08f8aac94d91b026127d06a3e81aa0c9dca
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-09-04 16:59:06 -0300

    botão de carregamento da tela fundos

--- src/screens/Fundos/index.tsx
+++ src/screens/Fundos/index.tsx
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Container, ListFunds } from "./styles";
 import { ResponseApiFundos } from "../../@types/typesApi";
 import { CardFundos } from "../../Components/CardFundos";
+import { ErrorScreen } from "../../Components/ErrorScreen";
 import { Load } from "../../Components/Load";
 import api from "../../services/api";
+
 export function Fundos() {
   const [listFunds, setListFunds] = useState<ResponseApiFundos[]>([]);
-  const [loadingFunds, setLoadingFunds] = useState(true);
+  const [loadingFunds, setLoadingFunds] = useState(false);
+
+  function handleButtonPress() {
+    setLoadingFunds(true);
+    setTimeout(() => {
+      setLoadingFunds(false);
+    }, 3000);
+  }
+
   async function getApi() {
     try {
       const responseF = await api.get("/funds");
@@ -20,17 +30,16 @@ export function Fundos() {
   useEffect(() => {
     getApi;
   }, []);
+
   return (
     <Container>
-      {loadingFunds ? (
-        <Load />
-      ) : (
-        <ListFunds
-          data={listFunds}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => <CardFundos fundos={item} />}
-        />
-      )}
+      <ErrorScreen isLoading={loadingFunds} onPress={handleButtonPress} />
+
+      <ListFunds
+        data={listFunds}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <CardFundos fundos={item} />}
+      />
     </Container>
   );
 }
