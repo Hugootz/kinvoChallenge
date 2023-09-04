c28250eb4bce2b2e967930c770907ff25025f658
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-09-04 16:58:08 -0300

    nova cor ao tema global

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -2,6 +2,7 @@ export default {
   colors: {
     purple_title: "#8c19ff",
     gray_title: "#b5b5b5",
+    gray_dark: "#808080",
     background: "#d3d3d3",
     dark: "#000000",
     light: "#ffffff",
