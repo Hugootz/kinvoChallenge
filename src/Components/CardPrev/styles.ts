655df1aef785f25f0d93bbcc65249861c8fb9c5e
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-09-06 13:52:39 -0300

    estilização dos componentes CardPrev

--- src/Components/CardPrev/styles.ts
+++ src/Components/CardPrev/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.View`
 `;
 export const Details = styled.View`
   width: 350px;
-  height: 180px;
+  height: 200px;
   background-color: ${({ theme }) => theme.colors.light};
   border-radius: 7px;
 `;
@@ -17,13 +17,60 @@ export const Name = styled.Text`
   font-weight: bold;
   color: ${({ theme }) => theme.colors.purple_title};
 `;
-export const Type = styled.Text``;
+export const Type = styled.Text`
+  color: ${({ theme }) => theme.colors.gray_dark};
+`;
+
+export const WrapperName = styled.View`
+  left: 8px;
+  top: 25px;
+`;
+export const ValorMinimo = styled.Text`
+  padding-bottom: 10px;
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+`;
+export const Taxa = styled.Text`
+  padding-bottom: 10px;
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+`;
+export const Resgate = styled.Text`
+  padding-bottom: 10px;
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+`;
+export const Rentabilidade = styled.Text`
+  padding-bottom: 10px;
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+`;
+
 export const WrapperDetails = styled.View`
   align-items: flex-end;
   right: 8px;
-  top: 30px;
+  bottom: 98px;
 `;
-export const MinimumValue = styled.Text``;
-export const Tax = styled.Text``;
-export const RedemptionTerm = styled.Text``;
-export const Profitability = styled.Text``;
+export const MinimumValue = styled.Text`
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+  padding-bottom: 10px;
+`;
+
+export const Tax = styled.Text`
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+  padding-bottom: 10px;
+`;
+
+export const RedemptionTerm = styled.Text`
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+  padding-bottom: 10px;
+`;
+
+export const Profitability = styled.Text`
+  color: ${({ theme }) => theme.colors.gray_dark};
+  font-weight: bold;
+  padding-bottom: 10px;
+`;
