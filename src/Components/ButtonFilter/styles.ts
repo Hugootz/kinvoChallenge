a30e4d15ac0f61191916dd8d2795fab22e6c1113
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-09-06 14:00:57 -0300

    mudança da cor e uso tema global

--- src/Components/ButtonFilter/styles.ts
+++ src/Components/ButtonFilter/styles.ts
@@ -1,13 +1,15 @@
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { ButtonFilterProps } from "../ButtonFilter";
+import theme from "../../global/theme";
 
 export const Container = styled.View``;
 export const ButtonWrapper = styled(TouchableOpacity)``;
 export const DetailsStyle = styled.View<ButtonFilterProps>`
   width: 100px;
   height: 40px;
-  background-color: ${({ isActive }) => (isActive ? "#000000" : "#ffffff")};
+  background-color: ${({ isActive }) =>
+    isActive ? theme.colors.purple_title : theme.colors.light};
   top: 15px;
   border-radius: 17px;
   margin-bottom: 30px;
@@ -15,7 +17,8 @@ export const DetailsStyle = styled.View<ButtonFilterProps>`
 `;
 export const Filter = styled.Text<ButtonFilterProps>`
   padding: 10px;
-  color: ${({ isActive }) => (isActive ? "#ffffff" : "#000000")};
+  color: ${({ isActive }) =>
+    isActive ? theme.colors.light : theme.colors.dark};
 
   font-weight: bold;
   align-self: center;
