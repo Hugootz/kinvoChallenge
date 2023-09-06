13137c8ff356b436bad566d979555d153d1c3239
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-09-06 13:48:41 -0300

    styled para ativar cor botão tipagem do componente

--- src/Components/ButtonFilter/styles.ts
+++ src/Components/ButtonFilter/styles.ts
@@ -1,27 +1,21 @@
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import { colorChange } from "../../@types/typesApi";
-
-interface StyledFilterProps {
-  activeTheme: boolean;
-}
+import { ButtonFilterProps } from "../ButtonFilter";
 
 export const Container = styled.View``;
 export const ButtonWrapper = styled(TouchableOpacity)``;
-export const DetailsStyle = styled.View<StyledFilterProps>`
+export const DetailsStyle = styled.View<ButtonFilterProps>`
   width: 100px;
   height: 40px;
-  background-color: ${({ activeTheme }) =>
-    activeTheme ? "#ffffff" : "#000000"};
-
+  background-color: ${({ isActive }) => (isActive ? "#000000" : "#ffffff")};
   top: 15px;
   border-radius: 17px;
   margin-bottom: 30px;
   margin: 10px;
 `;
-export const Filter = styled.Text<StyledFilterProps>`
+export const Filter = styled.Text<ButtonFilterProps>`
   padding: 10px;
-  color: ${({ activeTheme }) => (activeTheme ? "#000000" : "#ffffff")};
+  color: ${({ isActive }) => (isActive ? "#ffffff" : "#000000")};
 
   font-weight: bold;
   align-self: center;
