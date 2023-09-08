42742fe66d23511edc09dc58ad7e602c59ef53a6
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-09-08 10:31:35 -0300

    remoção de importações sem uso

--- src/screens/Fundos/index.tsx
+++ src/screens/Fundos/index.tsx
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Container, ListFunds } from "./styles";
-import { ResponseApiFundos } from "../../@types/typesApi";
-import { CardFundos } from "../../Components/CardFundos";
+
 import { ErrorScreen } from "../../Components/ErrorScreen";
-import { Load } from "../../Components/Load";
-import api from "../../services/api";
 
 export function Fundos() {
-  const [listFunds, setListFunds] = useState<ResponseApiFundos[]>([]);
   const [loadingFunds, setLoadingFunds] = useState(false);
 
   function handleButtonPress() {
